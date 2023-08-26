d6008645eb932fc4bb2e0bfdb5a2c5658ff98cac
Harshil Panchal <harshilpanchal00@gmail.com>
2023-08-25 18:31:29 -0700

    added container for add-job-btn

--- content-script.js
+++ content-script.js
@@ -17,16 +17,20 @@
 
         console.log(addJobBtnExists);
         if(!addJobBtnExists) {
+            const btnDiv = document.createElement("div");
             const addJobBtn = document.createElement("img");
-
+                        
             addJobBtn.src = chrome.runtime.getURL("images/job-btn.png");
             addJobBtn.className = "add-job-btn";
             addJobBtn.title = "Click to add to google sheets";
+            
+            btnDiv.className = "inline-flex btn-container";
+            btnDiv.appendChild(addJobBtn);
 
             linkedInBtnBar = document.getElementsByClassName('mt5')[2];
             console.log(linkedInBtnBar);
             if(linkedInBtnBar.firstElementChild) {
-                linkedInBtnBar.firstElementChild.appendChild(addJobBtn);
+                linkedInBtnBar.firstElementChild.appendChild(btnDiv);
             }
         }
     }
