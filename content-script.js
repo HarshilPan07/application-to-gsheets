2b162888913f30b668d6c9d2c539a308f10abf89
Harshil Panchal <harshilpanchal00@gmail.com>
2023-08-26 22:31:36 -0700

    added fetchAllJobs with chrome.storage

--- content-script.js
+++ content-script.js
@@ -1,9 +1,38 @@
 (() => {
     let linkedInBtnBar;
     let currentJob = "";
+    let user = null;
+    let allJobs = [];
+
+    const fetchAllJobs = () => {
+        console.log(user);
+        return new Promise((resolve) => {
+            chrome.storage.sync.get([user], (obj) => {
+                resolve(obj[user] ? JSON.parse(obj[user]) : []);
+            });
+        });
+    }
+
+    const addNewJobEventHandler = async () => {
+        if(!allJobs.includes(currentJob)) {
+            const addJobBtn = document.getElementsByClassName("add-job-btn")[0];
+        
+            addJobBtn.src = chrome.runtime.getURL("images/job-added-btn.png");
+            addJobBtn.className = "job-btn job-added-btn";
+            addJobBtn.title = "Click to remove from Google Sheets";
+            
+            allJobs = await fetchAllJobs();
+
+            chrome.storage.sync.set({
+                [user]: JSON.stringify([...allJobs, currentJob])
+            });
+        } else {
+            console.log('here');
+        }
+    }
 
     chrome.runtime.onMessage.addListener((obj, sender, res) => {
-
+        
         const { type, jobID } = obj;
 
         if(type === "NEW") {
@@ -12,34 +41,72 @@
         }
     });
 
-    const newJobLoaded = () => {
-        const addJobBtnExists = document.getElementsByClassName("add-job-btn")[0];
+    const newJobLoaded = async () => {
+        const addJobBtnExists = document.getElementsByClassName("job-btn")[0];
+        if(!user) {
+            await chrome.storage.sync.get(["user"]).then((res) => {
+                user = res["user"];
+            });
+        }
+        allJobs = await fetchAllJobs();
+        console.log(allJobs);
+        console.log(allJobs.includes(currentJob));
 
-        console.log(addJobBtnExists);
-        if(!addJobBtnExists) {
+        if(!allJobs.includes(currentJob) && !addJobBtnExists) {
             const btnDiv = document.createElement("div");
             const addJobBtn = document.createElement("img");
-                        
+
             addJobBtn.src = chrome.runtime.getURL("images/job-btn.png");
-            addJobBtn.className = "add-job-btn";
-            addJobBtn.title = "Click to add to google sheets";
+            addJobBtn.className = "job-btn add-job-btn";
+            addJobBtn.title = "Click to add to Google Sheets";
             
             btnDiv.className = "inline-flex btn-container";
             btnDiv.appendChild(addJobBtn);
 
             linkedInBtnBar = document.getElementsByClassName('mt5')[2];
-            console.log(linkedInBtnBar);
-            if(linkedInBtnBar.firstElementChild) {
+
+            if(linkedInBtnBar && linkedInBtnBar.firstElementChild) {
                 linkedInBtnBar.firstElementChild.appendChild(btnDiv);
             }
-
+            
+            
             addJobBtn.addEventListener("click", addNewJobEventHandler);
         }
+
+        // if(!addJobBtnExists) {                
+        //     const btnDiv = document.createElement("div");
+        //     const addJobBtn = document.createElement("img");
+
+        //     addJobBtn.src = chrome.runtime.getURL("images/job-btn.png");
+        //     addJobBtn.className = "job-btn add-job-btn";
+        //     addJobBtn.title = "Click to add to Google Sheets";
+            
+        //     btnDiv.className = "inline-flex btn-container";
+        //     btnDiv.appendChild(addJobBtn);
+
+        //     linkedInBtnBar = document.getElementsByClassName('mt5')[2];
+
+        //     if(linkedInBtnBar && linkedInBtnBar.firstElementChild) {
+        //         linkedInBtnBar.firstElementChild.appendChild(btnDiv);
+        //     }
+            
+            
+        //     addJobBtn.addEventListener("click", addNewJobEventHandler);
+        // }
     }
 })();
 
 
 /*
-mt5
-if div with display flex => can do it
+
+extension/browser opens:
+
+1. check if user already authorized
+2. depending on above, do either: 
+    2.1 user logs in + authorizes -> save auth data, create sheet, save to storage
+    2.2 extension retrieves info -> retrieve sheet data from storage
+3. 
+
+
+
 */
