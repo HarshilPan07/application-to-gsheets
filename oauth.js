d7d0b8a87a7597ec9503f2d6b4c6cff7fa191d83
Harshil Panchal <harshilpanchal00@gmail.com>
2023-08-27 23:20:25 -0700

    code in oauth.js provides email and spreadsheets :)

--- oauth.js
+++ oauth.js
@@ -2,24 +2,26 @@ window.onload = () => {
     chrome.identity.getAuthToken( {interactive : true}, async (token) => {
         console.log("token is " + token);
         await chrome.storage.sync.set({ "user" : token })
-        // .then(() => {
-        //     let init = {
-        //         method: 'GET',
-        //         async: true,
-        //         headers: {
-        //             "Authorization": 'Bearer ' + token,
-        //             "Content-Type": 'application/json'
-        //             },
-        //         "contentType": "json"
-        //         };
-         
-        //     fetch("https://www.googleapis.com/auth/userinfo.email", init)
-        //         .then((response) => response.json())
-        //         .then((data) => {
-        //         console.log(data);
-        //         console.log('here');
-        //     });
-        // });
+        chrome.identity.getProfileUserInfo({ accountStatus: 'ANY' }, function (user_info) {
+            console.log(user_info.email);
+        });
+        
+        let fetch_options = {
+            method: 'GET',
+            async: true,
+            headers: {
+              Authorization: 'Bearer ' + token,
+              'Content-Type': 'application/json'
+            },
+            'contentType': 'json'
+          };
+          fetch(
+              "https://sheets.googleapis.com/v4/spreadsheets/1Io34OGN4Vl051rq_PpXdpyRrnE8mxozdiAfGJm_FQKg",
+              fetch_options)
+              .then((response) => response.json())
+              .then(function(data) {
+                console.log(data)
+              });
     });
   };
 
