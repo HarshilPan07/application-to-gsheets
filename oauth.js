98f4f74078a268f04080582a890c4847238d657a
Harshil Panchal <harshilpanchal00@gmail.com>
2023-08-27 23:27:58 -0700

    stable

--- oauth.js
+++ oauth.js
@@ -6,22 +6,22 @@ window.onload = () => {
             console.log(user_info.email);
         });
         
-        let fetch_options = {
-            method: 'GET',
-            async: true,
-            headers: {
-              Authorization: 'Bearer ' + token,
-              'Content-Type': 'application/json'
-            },
-            'contentType': 'json'
-          };
-          fetch(
-              "https://sheets.googleapis.com/v4/spreadsheets/1Io34OGN4Vl051rq_PpXdpyRrnE8mxozdiAfGJm_FQKg",
-              fetch_options)
-              .then((response) => response.json())
-              .then(function(data) {
-                console.log(data)
-              });
+        // let fetch_options = {
+        //     method: 'GET',
+        //     async: true,
+        //     headers: {
+        //       Authorization: 'Bearer ' + token,
+        //       'Content-Type': 'application/json'
+        //     },
+        //     'contentType': 'json'
+        //   };
+        //   fetch(
+        //       "https://sheets.googleapis.com/v4/spreadsheets/1Io34OGN4Vl051rq_PpXdpyRrnE8mxozdiAfGJm_FQKg",
+        //       fetch_options)
+        //       .then((response) => response.json())
+        //       .then(function(data) {
+        //         console.log(data)
+        //       });
     });
   };
 
--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,3 @@
-const API_KEY = "";
-let user_signed_in = false;
-
-chrome.identity.onSignInChanged.addListener((account_id, signedIn) => {
-    user_signed_in = signedIn ? true : false;
-});
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-
-});
-
 chrome.tabs.onUpdated.addListener((tabID, changeInfo, tab) => {
     if(tab.url && changeInfo.status === "complete" && tab.url.includes("linkedin.com/jobs/")) {
         const queryParameter = tab.url.split("?")[1];
