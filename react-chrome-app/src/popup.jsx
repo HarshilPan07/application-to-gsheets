ad600006b78c6b9e4986baaf30170177865d2088
Harshil Panchal <harshilpanchal00@gmail.com>
2023-09-01 23:39:41 -0700

    added basic layout and grid styling

--- react-chrome-app/src/popup.jsx
+++ react-chrome-app/src/popup.jsx
@@ -4,12 +4,38 @@ import './popup.css';
 
 function Popup() {
     return (
-        <main>
-            <div>
-                <h1>Hello, world!</h1>
-                <p>This is a simple popup.</p>
+        <>
+            <div id='top-container'>
+                <div id='top-jobs-applied-today'>
+                    5 applied today
+                </div>
+                <div id='top-total-jobs-applied'>
+                    25 applied total
+                </div>
             </div>
-        </main>
+            <div id='middle-container'>
+                <div id='middle-left-metric'>
+                    metric 1
+                </div>
+                <div id='middle-right-metric'>
+                    metric 2
+                </div>
+            </div>
+            <div id='bottom-container'>
+                <div id='bottom-title'>
+                    Most Recently Applied
+                </div>
+                <div id='most-recent-jobs-list'>
+                    <ul>
+                        <li>job1</li>
+                        <li>job2</li>
+                        <li>job3</li>
+                        <li>job4</li>
+                        <li>job5</li>
+                    </ul>
+                </div>
+            </div>
+        </>
     );
 }
 
